399b3304040b1b802052a9e6381aaf8f55a29620
Kaushik kundu <kaushikkundu774@gmail.com>
2025-10-04 22:43:24 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { Header } from "@/components/header"
 export default function DashboardPage() {
   return (
     <div className="flex h-screen bg-background">
-      <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
         <main className="flex-1 p-6 overflow-auto">
